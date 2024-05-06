15c008a249c85d60f2dec58153a62b8f223948da
sohilsk9340 <sohilkhansk942@gmail.com>
2024-05-06 12:40:02 +0530

    todo app API

---
+++ db/user.js
@@ -0,0 +1,14 @@
+const { default: mongoose } = require("mongoose");
+const todoSchema = require("./todoSchema");
+const userSchema = require("./userSchema");
+
+const DB_NAME = process.env.DB_NAME || "todo";
+const URI = process.env.MONGO_URI || "mongodb+srv://todo:todo@cluster0.elwmgmk.mongodb.net/todo";
+const MONGO_URI = `${URI}/${DB_NAME}`;
+
+mongoose.connect(MONGO_URI).then(() => console.log("Connected")).catch((err) => console.log(err))
+
+const users = mongoose.model('user', userSchema, 'users');
+const todos = mongoose.model('todo', todoSchema, 'todos');
+
+module.exports = {users, todos};
