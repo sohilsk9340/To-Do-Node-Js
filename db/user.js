de12c2bf9e701b23e285750d91ed2ce11e075f65
sohilsk9340 <sohilkhansk942@gmail.com>
2024-05-06 14:38:08 +0530

    new update

--- db/user.js
+++ db/user.js
@@ -3,7 +3,7 @@ const todoSchema = require("./todoSchema");
 const userSchema = require("./userSchema");
 
 const DB_NAME = process.env.DB_NAME || "todo";
-const URI = process.env.MONGO_URI || "mongodb+srv://todo:todo@cluster0.elwmgmk.mongodb.net/todo";
+const URI = process.env.MONGO_URI || "mongodb+srv://todo:todo@cluster0.elwmgmk.mongodb.net";
 const MONGO_URI = `${URI}/${DB_NAME}`;
 
 mongoose.connect(MONGO_URI).then(() => console.log("Connected")).catch((err) => console.log(err))
