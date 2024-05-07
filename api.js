c09fd59f2fd1383d80eeab848c33243dc8a57641
sohilsk9340 <sohilkhansk942@gmail.com>
2024-05-07 09:53:45 +0530

    update 7

--- api.js
+++ api.js
@@ -67,4 +67,19 @@ app.post('/user/create', async (req, res) => {
             : res.status(500).json({msg: "Error", data: newUser})
 })
 
+// Login
+
+app.post("/login", (req, res) => {
+    const body = req.body;
+    const username = body.username;
+    const pass = body.pass;
+
+    if(username === "sohil" && pass === 123)
+        res.json({
+            data: "success",
+        })
+    else 
+        res.end("Incorrect creds")
+})
+
 app.listen(PORT, () => console.log(`Application listening on port ${PORT}!`))
